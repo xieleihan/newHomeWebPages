b2f0ec8454529f5aafd6434d7111d55128f381e9
xieleihan <xieleihan@gmail.com>
2025-02-27 09:55:19 +0800

    feat:继续更新

--- src/components/Modules/home/HomeheaderCom.tsx
+++ src/components/Modules/home/HomeheaderCom.tsx
@@ -1,17 +1,13 @@
 // 导入Antd
-import { Layout,Menu } from 'antd';
-import type { MenuProps } from 'antd';
+import { Layout } from 'antd';
+
+// 导入图片
+import Settings from '../../../assets/icon/setting.svg';
+import Sider from '../../../assets/icon/sider.svg';
+import Translate from '../../../assets/icon/translate.svg';
 
 function HomeheaderCom() {
     const { Header } = Layout;
-
-    type MenuItem = Required<MenuProps>['items'][number];
-    const items: MenuItem[] = [
-        {
-            label: '首页',
-            key: 'mail',
-        },
-    ]
 
     return (
         <>
@@ -21,8 +17,19 @@ function HomeheaderCom() {
                     <h1 className='title'>南秋SouthAki的个人主页</h1>
                 </div>
                 <nav className="right">
-                    <Menu className='nav' mode="horizontal" items={items} />
+                    <ul className='nav'>
+                        <li className='item'>首页</li>
+                        <li className='item'>项目</li>
+                        <li className='item'>关于</li>
+                    </ul>
+                    <div className="operateBox">
+                        <img loading="lazy" className='icon' src={Sider} alt="侧边栏" />
+                        <img loading="lazy" className='icon' src={Settings} alt="设置" />
+                        <img loading="lazy" className='icon' src={Translate} alt="翻译" />
+                    </div>
                 </nav>
+
+                <div className="popup"></div>
             </Header>
         </>
     );
