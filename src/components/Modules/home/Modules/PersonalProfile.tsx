f642a0a62fa15f94405e734d69bada3cfb6522c9
xieleihan <xieleihan@gmail.com>
2025-03-05 18:32:16 +0800

    feat:继续推进

--- src/components/Modules/home/Modules/PersonalProfile.tsx
+++ src/components/Modules/home/Modules/PersonalProfile.tsx
@@ -7,6 +7,12 @@ import { useEffect, useRef } from "react";
 // 导入waterCode
 import waterCode from "../../../../utils/waterCode";
 
+// 导入图片
+import avater from "../../../../assets/images/avater.png";
+
+// 导入Antd design组件
+import {Button} from "antd";
+
 function PersonalProfile() {
     const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
@@ -27,10 +33,33 @@ function PersonalProfile() {
     return (
         <>
             <section className={styles.personalProfile}>
+                {/* 背景canvas */}
                 <canvas ref={canvasRef} className={styles.canvas}></canvas>
 
+                {/* 内容区域 */}
                 <div className={styles.container}>
-                    
+                    <div className={styles.leftTop}>
+                        {/* 头像 */}
+                        <div className={styles.avaterBox}>
+                            <img className={styles.avater} loading='lazy' src={avater} alt="头像" />
+                        </div>
+                        <div className={styles.username}>
+                            南秋SouthAki
+                        </div>
+                        {/* 个性签名 */}
+                        <div className={styles.signature}>
+                            循此苦旅,以达星辰
+                        </div>
+                        {/* 按钮区域 */}
+                        <div className={styles.btnBox}>
+                            <Button>Github</Button>
+                            <Button>Blog</Button>
+                            <Button>WeChat</Button>
+                        </div>
+                    </div>
+                    <div className={styles.rightBottom}>
+
+                    </div>
                 </div>
             </section>
         </>
