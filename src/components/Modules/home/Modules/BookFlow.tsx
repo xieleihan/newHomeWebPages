9b82995a440efaadf59f172f78e3c4cb81765c17
xieleihan <xieleihan@gmail.com>
2025-03-12 11:51:51 +0800

    feat:swiper的支持

--- src/components/Modules/home/Modules/BookFlow.tsx
+++ src/components/Modules/home/Modules/BookFlow.tsx
@@ -1,16 +1,35 @@
 // 导入布局组件
 import ComponentsLayout from "../../../../layout/ComponentsLayout";
 
+// 导入样式
+import styles from '../../../../style/home/BookFlow.module.scss';
+
+// 导入React
+import { useEffect } from "react";
+
+// 导入Antd design组件
+import {Button} from 'antd';
+
 function BookFlow() {
+    // 创建生命周期
+    useEffect(() => {
+        
+    }, []);
+
     return (
         <>
             <ComponentsLayout
-                title="书库"
+                title="个人书库"
                 isOpenPagination={false}
                 container={
                     <>
-                        <h3>图书流程</h3>
-                        <p>图书流程图</p>
+                        <div className={styles.bookBox}>
+                            <p className={styles.smallTitle}>点击看看</p>
+                            <div className={styles.content}>
+
+                            </div>
+                            <Button>查看更多</Button>
+                        </div>
                     </>
                 }
             ></ComponentsLayout>
