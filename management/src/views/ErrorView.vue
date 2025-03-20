0d35fba381f67a7dae13ed3bd14b038ea9a32585
xieleihan <xieleihan@gmail.com>
2025-03-21 00:54:05 +0800

    fix:缓慢更新

--- management/src/views/ErrorView.vue
+++ management/src/views/ErrorView.vue
@@ -5,10 +5,15 @@
                 <img loading="lazy" src="../assets/icon/404.svg" alt="Error">
             </div>
             <div class="bottomRight">
-                <p class="title">网络错误</p>
-                <p class="desc">你来到无人问津的荒漠</p>
-                <el-button type="primary">返回首页</el-button>
+                <div class="box">
+                    <p class="title">网络错误</p>
+                    <p class="desc">你来到无人问津的荒漠</p>
+                </div>
+                <el-button size="large" type="primary">返回首页</el-button>
             </div>
+        </div>
+        <div class="copyright">
+            Copyright© 2025 SouthAki, All Rights Reserved. | 
         </div>
     </div>
 </template>
@@ -24,10 +29,57 @@
     display: flex;
     justify-content: center;
     align-items: center;
+    font-family: 'zk';
+    position: relative;
+    .copyright{
+        position: absolute;
+        bottom: 0;
+        left: 50%;
+        transform: translateX(-50%);
+        font-size: .1rem;
+
+    }
     .container{
         width: 60%;
         height: 60%;
-        
+        display: flex;
+
+        .topLeft{
+            width: 50%;
+            height: 100%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            img{
+                width: 100%;
+                height: 100%;
+            }
+        }
+        .bottomRight{
+            width: 50%;
+            height: 100%;
+            display: flex;
+            justify-content: space-evenly;
+            align-items: center;
+            flex-direction: column;
+            .box{
+                width: 100%;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                flex-direction: column;
+                .title{
+                    font-size: .3rem;
+                    font-weight: bold;
+                }
+                .desc{
+                    font-size: .1rem;
+                    letter-spacing: .1rem;
+                    color: #ccc;
+                    margin-top: .1rem;
+                }
+            }
+        }
     }
 }
 </style>
